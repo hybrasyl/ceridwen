b30e4f0f5893702ad59a38ddf88d762db60b3102
Caeldeth <sabrael@hybrasyl.com>
2024-07-24 21:35:47 -0500

    Update placeholder scripts to not throw errors

--- scripts/castable/AutoHemloch.lua
+++ scripts/castable/AutoHemloch.lua
@@ -1,1 +1,10 @@
-0
+-- SkillAutoHemloch
+-- Uses a hemloch automatically or some other dumb medenia bullshit
+
+--  -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~-  --
+--  OnUse
+--  -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~-  --
+
+function OnUse()
+  source.SystemMessage("This feature is not yet implemented!")
+end
--- scripts/castable/BalanceMana.lua
+++ scripts/castable/BalanceMana.lua
@@ -1,1 +1,10 @@
-0
+-- BalanceMana
+-- Lylican plant or some other dumb medenia bullshit
+
+--  -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~-  --
+--  OnUse
+--  -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~- -~*~-  --
+
+function OnUse()
+  source.SystemMessage("This feature is not yet implemented!")
+end
